cffef302b0807c8204f69da132e3dcbeaa989592
Taetae1123 <xodus2079@naver.com>
2025-03-20 03:34:28 +0900

    Fix: 가운데 정렬

---
+++ react-test2/src/RightInfo.jsx
@@ -0,0 +1,11 @@
+const RightInfo = () => {
+  return (
+    <div>
+      <div>적립</div>
+      <div>최대 3,050원 쿠팡캐시 적립 · 쿠페이 머니 결제시 </div>
+      <div>혜택보기 {">"}</div>
+    </div>
+  );
+};
+
+export default RightInfo;
